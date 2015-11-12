21a41e30a366452fe1552e0b87f0f54eb7a9bf42
zusdn90 <zusdn90@naver.com>
2015-11-12 15:56:29 +0900

    Transfer

---
+++ src/test/java/com/hybrid/transfer/CityTransfer.java
@@ -0,0 +1,40 @@
+package com.hybrid.transfer;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.hybrid.mapper.CityMapper;
+import com.hybrid.model.City;
+
+public class CityTransfer {
+	
+	static Log log = LogFactory.getLog(CityTransfer.class);
+	
+	public static void main(String[] args) {
+		GenericApplicationContext ctx = null;
+		ctx = new GenericXmlApplicationContext("spring/beans_mysql.xml","spring/beans_oracle.xml");
+		
+		CityMapper mysqlCityMapper = (CityMapper) ctx.getBean("mysqlCityMapper");
+	//	CityMapper oracleCityMapper = (CityMapper) ctx.getBean("oracleCityMapper");
+		
+		List<City> list = mysqlCityMapper.selectAll();
+		
+		log.info("city size = " + list.size());
+		
+		list.forEach(new Consumer<City>()
+				{
+
+					@Override
+					public void accept(City t) {
+				//		oracleCityMapper.insert(t);
+					}
+				}
+				);
+		ctx.close();
+	}
+}
